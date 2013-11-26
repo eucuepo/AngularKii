d8bae828b04f2c5f41e54823f82370521263b320
Arda Karaduman <arda.karaduman@kii.com>
2013-11-26 15:09:59 +0900

    move add function to issues controller

--- app/scripts/controllers/issues.js
+++ app/scripts/controllers/issues.js
@@ -26,5 +26,62 @@ angular.module('angularKiiApp')
 	  $scope.fetchIssues();
       };
 
+    $scope.createIssue = function(){
+		var issue = {
+			title : $scope.issue.title,
+			description : $scope.issue.description
+		}
+
+		// Create the Issue object
+		kiiService.createObject(issue)
+			.then(
+				function(user){
+					$scope.successMessage = 'Issue created';
+					delete $scope.errorMessage;
+				}, function(error){
+					$scope.errorMessage = error;
+				}
+			); 
+	};
+
+
+	// datepicker stuff
+	$scope.today = function() {
+	    $scope.dt = new Date();
+	  };
+	  $scope.today();
+
+	  $scope.showWeeks = true;
+	  $scope.toggleWeeks = function () {
+	    $scope.showWeeks = ! $scope.showWeeks;
+	  };
+
+	  $scope.clear = function () {
+	    $scope.dt = null;
+	  };
+
+	  // Disable weekend selection
+	  $scope.disabled = function(date, mode) {
+	    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+	  };
+
+	  $scope.toggleMin = function() {
+	    $scope.minDate = ( $scope.minDate ) ? null : new Date();
+	  };
+	  $scope.toggleMin();
+
+	  $scope.open = function() {
+	    $timeout(function() {
+	      $scope.opened = true;
+	    });
+	  };
+
+	  $scope.dateOptions = {
+	    'year-format': "'yy'",
+	    'starting-day': 1
+	  };
+
+	  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+	  $scope.format = $scope.formats[0];
 
   });
